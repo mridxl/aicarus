09a8da8470a069a5e6b8976beaaee41cb45829a0
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-12-16 16:20:19 +0530

    Add createProject and getProjects endpoints

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -5,6 +5,7 @@ import { pollCommits } from "@/lib/github";
 import { indexGithubRepo } from "@/lib/github-loader";
 
 export const projectRouter = createTRPCRouter({
+  // Create a new project for the current user
   createProject: protectedProcedure
     .input(
       z.object({
@@ -18,7 +19,7 @@ export const projectRouter = createTRPCRouter({
         data: {
           name: input.name,
           githubUrl: input.githubUrl,
-          UserToProject: {
+          userToProject: {
             create: {
               // setting ! (non-null assertion operator) because we know the user is authenticated
               userId: ctx.user.userId!,
@@ -30,10 +31,12 @@ export const projectRouter = createTRPCRouter({
       await indexGithubRepo(project.id, input.githubUrl, input.githubToken);
       return project;
     }),
+
+  // Get all projects for the current user
   getProjects: protectedProcedure.query(async ({ ctx }) => {
     return ctx.db.project.findMany({
       where: {
-        UserToProject: {
+        userToProject: {
           some: {
             userId: ctx.user.userId!,
           },
@@ -41,6 +44,8 @@ export const projectRouter = createTRPCRouter({
       },
     });
   }),
+
+  // Get the commits for a project
   getCommits: protectedProcedure
     .input(
       z.object({
@@ -52,6 +57,49 @@ export const projectRouter = createTRPCRouter({
       return ctx.db.commit.findMany({
         where: {
           projectId: input.projectId,
+        },
+      });
+    }),
+
+  // Save the answer to a question
+  saveAnswer: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+        question: z.string(),
+        answer: z.string(),
+        filesReferences: z.any(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.question.create({
+        data: {
+          userId: ctx.user.userId!,
+          projectId: input.projectId,
+          question: input.question,
+          answer: input.answer,
+          filesReferences: input.filesReferences,
+        },
+      });
+    }),
+
+  // Get all questions for a project
+  getQuestions: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.question.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
         },
       });
     }),
